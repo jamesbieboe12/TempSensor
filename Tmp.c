3b7c8724f11f42dc5a3d48218a8083280b925e31
jamesbieboe12 <47893229+jamesbieboe12@users.noreply.github.com>
2019-04-25 19:09:25 -0400

    Newer version

--- Tmp.c
+++ Tmp.c
@@ -1,3 +1,4 @@
+#include <SPI.h>
 #include <Arduino.h>
 #include <Wire.h>
 #include <ArdusatSDK.h>
@@ -7,13 +8,17 @@ ArdusatSerial serialConnection(SERIAL_MODE_HARDWARE_AND_SOFTWARE, 8, 9);
 Luminosity lum;
 TemperatureMLX temp;
 
+Display display;
+
 void setup(void)
 {
   serialConnection.begin(9600);
 
   lum.begin();
   temp.begin();
-
+  
+display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  
   serialConnection.println("");
 }
 
@@ -21,15 +26,16 @@ void loop(void)
 {
   serialConnection.println(temp.readToJSON("infraredTemp"));
   serialConnection.println(lum.readToJSON("luminosity"));
+  
 
  if (temp.t > 15) {  
-  display.drawPixel(2, 5, WHITE);
-  display.drawPixel(3, 6, WHITE);
-  display.drawPixel(4, 7, WHITE);
-  display.drawPixel(5, 7, WHITE);
-  display.drawPixel(6, 7, WHITE);
-  display.drawPixel(7, 7, WHITE);
-  display.drawPixel(8, 7, WHITE);
+ 
+  display.clearDisplay();
+ 
+  display.drawRect(14, 4, 15, 11, WHITE);
+  display.drawLine(11,9,19,13, WHITE);
+  display.drawLine(19,13,31,0, WHITE);
+ 
   display.display();
  }
 // else{}
