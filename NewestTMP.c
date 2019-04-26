cbf7389af467d2890fce3266b03697865266ff3b
jamesbieboe12 <47893229+jamesbieboe12@users.noreply.github.com>
2019-04-26 09:54:09 -0400

    Final version

--- NewestTMP.c
+++ NewestTMP.c
@@ -1,3 +1,4 @@
+
 #include <SPI.h>
 #include <Arduino.h>
 #include <Wire.h>
@@ -13,7 +14,7 @@ Display display;
 void setup(void)
 {
   serialConnection.begin(9600);
-// turns on temp meter and luminosity meter
+//turns on temp and luminosity meter
   lum.begin();
   temp.begin();
   //turns on oled
@@ -24,13 +25,12 @@ display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
 void loop(void)
 {
-  //shows things on serial monitor dor debugging
+  //serial monitor for debugging
   serialConnection.println(temp.readToJSON("infraredTemp"));
   serialConnection.println(lum.readToJSON("luminosity"));
   
-//the if statement underneath triggers when the temperature reads above the //value and displays on oled 
+//triggers when tmp>70 and displays check mark on oled 
  if (temp.t > 70) {  
- //this code draws a check mark
   display.clearDisplay();
  
   display.drawRect(14, 4, 15, 11, WHITE);
